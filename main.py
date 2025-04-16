b153a49f0aa83ef66be6cd3523a6013dc990fe9a
swan / crumb <138329745+crumbbdev@users.noreply.github.com>
2025-04-15 23:08:06 -0400

    Upload Files (Final Commit)

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+def set_discord_status(token, status):
+    url = "https://discord.com/api/v8/users/@me/settings"
+    headers = {
+        "Authorization": token,
+        "Content-Type": "application/json"
+    }
+    data = {
+        "status": "online",  # idle, dnd, invisible, online
+        "custom_status": {
+            "text": status
+        }
+    }
+    response = requests.patch(url, headers=headers, json=data)
+    
+    if response.status_code == 200:
+        print(f"Status Updated: {status}")
+    else:
+        print(f"Failed to update status... Error Code: {response.status_code} - {response.text}")
+
+token = "token"
+status = "Hello World!"
+
+set_discord_status(token, status)
